39de750a7916488f95c685c124ff9be60275dfed
Nasir-Raza <send2raza@gmail.com>
2022-07-23 16:39:31 +0500

    Second commit to JS-Assignment-14 Word matching game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var scoreMatched = 0, scoreMismatched = 0;
 var wordsList = [];
+var isAlreadyExists = false; 
 
 let dictionary = [
     "cat",
@@ -26,7 +27,7 @@ let dictionary = [
     "candle",
     "ali",
     "raza",
-    "azhar",
+    "nasir",
     "sara",
     "alice",
     "google",
@@ -57,26 +58,40 @@ function setInput(characters) {
 
 function matchWord() {
     var inputString = document.getElementById("input").value
+    
+    for (var i = 0; i < wordsList.length; i++) {
+        if (inputString.toLowerCase() === wordsList[i].slice(0, wordsList[i].length - 1).toLowerCase()) {
+            isAlreadyExists = true;
+            break;
+        }
+        else {
+            isAlreadyExists = false;
+        }
+    } 
     if (inputString.length > 2) {
-        
-        for (var i = 0; i < dictionary.length; i++) {
-            if (inputString.toLowerCase() === dictionary[i]) {
-                scoreMatched++;
-                wordsList.push(inputString + "M");
-                populateList();
-                break;
-            }
-            else {
-                if (i === dictionary.length - 1) {
-                    scoreMismatched++;
-                    wordsList.push(inputString + "N");
+        if (!isAlreadyExists){
+            for (var i = 0; i < dictionary.length; i++) {
+                if (inputString.toLowerCase() === dictionary[i]) {
+                    scoreMatched++;
+                    wordsList.push(inputString + "M");
                     populateList();
+                    break;
+                }
+                else {
+                    if (i === dictionary.length - 1) {
+                        scoreMismatched++;
+                        wordsList.push(inputString + "N");
+                        populateList();
+                    }
                 }
             }
         }
+        else {
+            alert(inputString + " already created and exists in the list.");    
+        }
     }
     else {
-        alert("Please enter atleast three characters string.")
+        alert("Please enter atleast three characters string.");
     }
 
     document.getElementById("wordmatched").innerText = "Words Matched: " + scoreMatched;
@@ -86,25 +101,25 @@ function matchWord() {
 }
 
 function resetGame() {
-    document.getElementById("input").value = "";
+    isAlreadyExists = false;
     scoreMatched = 0;
     scoreMismatched = 0;
+    wordsList.length = 0;
     document.getElementById("wordmatched").innerText = "Words Matched: " + scoreMatched;
     document.getElementById("wordmismatched").innerText = "Words Mismatched: " + scoreMismatched;
+    document.getElementById("input").value = "";
     document.getElementById("wordslist").innerHTML = "";
 }
 
 function populateList() {
-    
-
     document.getElementById("wordslist").innerHTML = "";
     for (var i = 0; i < wordsList.length; i++) {
         
         if (wordsList[i].charAt(wordsList[i].length - 1) === "M" ) {
-            document.getElementById("wordslist").innerHTML += "<tr><td class='" + "wordmatched" + "'>" + wordsList[i].substring(0, wordsList[i].length - 1) + "</td></tr>"
+            document.getElementById("wordslist").innerHTML += "<tr><td class='" + "wordmatched1" + "'>" + wordsList[i].substring(0, wordsList[i].length - 1) + "</td><td class='" + "wordmatched2" + "'><img class='wordmatched' src='./assets/right.png' /></td></tr>"
         }
         else {
-            document.getElementById("wordslist").innerHTML += "<tr><td class='" + "wordmismatched" + "'>" + wordsList[i].substring(0, wordsList[i].length - 1) + "</td></tr>"
+            document.getElementById("wordslist").innerHTML += "<tr><td class='" + "wordmismatched1" + "'>" + wordsList[i].substring(0, wordsList[i].length - 1) + "</td><td class='" + "wordmismatched2" + "'><img class='wordmismatched' src='./assets/wrong.png' /></td></tr>"
         }
     }
 }
